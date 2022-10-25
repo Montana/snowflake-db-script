a2309df0d2772c9912b7ac1d04f299f745a57e37
Montana Mendy <montana@linux.com>
2022-10-24 17:58:52 -0700

    push snowflake script

---
+++ snowflake_db.py
@@ -0,0 +1,21 @@
+class SnowflakeDatabase(SnowflakeResource):
+    def __init__(self, **kwargs):
+
+        self.name = kwargs["name"]
+        self.owner = kwargs["owner"]
+        self.comment = kwargs["comment"]
+        self.tf_filename = "snowflake_database.tf"
+        super().__init__(**kwargs)
+
+    @property
+    def snowflake_provider_resource(self):
+
+        return "snowflake_database"
+
+    @property
+    def resource_attributes(self):
+
+        return {
+            "name": stringify(self.name),
+            "comment": stringify(self.comment),
+        }
